9ec9be3561a63392f03ac9a9aa59e2a1dea4693d
Stmated <pontus.eliason@gmail.com>
2024-04-08 17:51:53 +0200

    Renamed duo- packages to test- and updated versions

--- packages/core/src/transform/AstTransformer.ts
+++ packages/core/src/transform/AstTransformer.ts
@@ -1,7 +1,7 @@
-import {AstNode} from '../ast';
+import {RootAstNode} from '../ast';
 import {AstTransformerArguments} from './AstTransformerArguments';
 import {TargetOptions} from '../interpret';
 
-export interface AstTransformer<TRoot extends AstNode = AstNode, TOpt extends TargetOptions = TargetOptions> {
+export interface AstTransformer<TRoot extends RootAstNode = RootAstNode, TOpt extends TargetOptions = TargetOptions> {
   transformAst(args: AstTransformerArguments<TRoot, TOpt>): void;
 }
